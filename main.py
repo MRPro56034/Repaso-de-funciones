7f6c7944f0f8f22ac02ee1df9af5447b5edbdfce
AaronArteta <aaronarteta14@gmail.com>
2024-10-21 15:57:29 -0500

    Create main.py
    parte uno

---
+++ main.py
@@ -0,0 +1,13 @@
+def double_letter(str):
+    result = ''
+    for letter in str:
+        result += letter * 2
+    return result
+
+def secret_function(a, b):
+    # Escriba aquí su código
+    return a + b
+
+print(Hola)
+print(secret_function(1, 2))
+print(secret_function("¡Hola, ", "Mundo!"))
